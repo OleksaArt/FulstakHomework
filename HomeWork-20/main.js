dafcbf93461f5fab0001a08f9f19f43cb4744d78
Oleksa <art.studios.ua@gmail.com>
2025-11-06 16:28:04 +0100

    update

--- HomeWork-20/main.js
+++ HomeWork-20/main.js
@@ -102,41 +102,25 @@ console.log(evenFn(20));
  * '2' → null
  * У реалізації функції обов'язково мають бути використані оператори switch / case / default.
  */
-// function weekFn(n){
-//   const result = [];
-//   switch (n) {
-//       case 1:
-//           console.log("Понеділок");
-//           break;
-//       case 2:
-//           console.log("Вівторок");
-//           break;
-//       case 3:
-//           console.log("Середа");
-//           break;
-//       case 4:
-//         console.log("Четвер");
-//         break;
-//       case 5:
-//         console.log("П'ятниця");
-//         break;
-//       case 6:
-//         console.log("Cубота");
-//         break;
-//       case 7:
-//         console.log("Неділя");
-//         break;
-//       default:
-//           console.log(null);
-//   }
-//   return result;
-// }
-// console.log(weekFn(1.2))   // 'Понеділок'
-// console.log(weekFn(3))   // 'Середа'
-// console.log(weekFn(7))   // 'Неділя'
-// console.log(weekFn(9))   // null
-// console.log(weekFn(1.5)) // null
-// console.log(weekFn('2')) // null
+function weekFn(n) {
+  switch (n) {
+    case 1: return 'Понеділок';
+    case 2: return 'Вівторок';
+    case 3: return 'Середа';
+    case 4: return 'Четвер';
+    case 5: return 'Пʼятниця';
+    case 6: return 'Субота';
+    case 7: return 'Неділя';
+    default: return null;
+  }
+}
+
+console.log(weekFn(1.2))   // 'Понеділок'
+console.log(weekFn(3))   // 'Середа'
+console.log(weekFn(7))   // 'Неділя'
+console.log(weekFn(9))   // null
+console.log(weekFn(1.5)) // null
+console.log(weekFn('2')) // null
 
 /*
  * #6
