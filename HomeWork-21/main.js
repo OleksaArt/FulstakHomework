e12297ff75f438f2e80c5673d2e23f553f40b5f6
Oleksa <art.studios.ua@gmail.com>
2025-11-09 22:52:42 +0100

    add homework №21

---
+++ HomeWork-21/main.js
@@ -0,0 +1,36 @@
+// function createGreeting(greeting) {
+//     return function(name) {
+//         return greeting + ', ' + name + '!';
+//     };
+// }
+
+// var greetHello = createGreeting('Hello');
+// var greetHi = createGreeting('Hi');
+
+// console.log(greetHello('Alice')); // Виведе: "Hello, Alice!"
+// console.log(greetHi('Bob')); // Виведе: "Hi, Bob!"
+
+user = {
+    name: "Дем'ян",
+    age: 20,
+    strenght: 100,
+    health: 100,
+    defense: 50,
+}
+goblin = {
+    name: "Тимуджин",
+    age: 30,
+    strenght: 35,
+    health: 150,
+    defense: 40,
+}
+
+function createDamage(strenght, health, defense){
+    let damage;
+    if(strenght === defense){
+        return damage = 0;
+    }else if(strenght > defense){
+        damage = strenght - defense;
+        return health - damage;
+    };
+};
